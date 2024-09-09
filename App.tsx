320309b6ca3c61d3e89e6b641e4dbf39597e2e12
shamila37 <128484072+shamila37@users.noreply.github.com>
2024-09-09 20:13:15 +0530

    Complete HeaderTabs

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
 import { Text, View } from "react-native";
+import Home from "./screens/Home";
 
 export default function App(){
   return (
-    <View
-      style = {{
-        flex:1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Uber Eats Clone App with React Native CLI</Text>
-    </View>
+    <Home />
   );
 }
---
+++ screens/Home.tsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import HeaderTab from '../components/HeaderTabs'
+import HeaderTabs from '../components/HeaderTabs'
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HeaderTabs/>
+    </SafeAreaView>
+  )
+}
